def1a96028fec8488805fa2c8746962a2a7b99a7
Ryutaro Kurai <ryu.kurai@gmail.com>
2011-07-31 17:51:21 +0900

    連載18回

--- Resources/android/testservice.js
+++ Resources/android/testservice.js
@@ -1,13 +1,42 @@
 var service = Titanium.Android.currentService;
 var intent = service.intent;
-var url = intent.getStringExtra("download_url");
+var twitter_id  = intent.getStringExtra("twitter_id");
+var url = 'http://api.twitter.com/1/statuses/user_timeline.json?screen_name=' + twitter_id;
 Ti.API.debug("Try Download: " + url);
 
 var xhr = Ti.Network.createHTTPClient();
 
+function showNotification(modified){
+	Ti.API.info('start notification');
+	var twitter_url = 'http://twitter.com/' + twitter_id;
+	var intent = Ti.Android.createIntent({
+		action: Ti.Android.ACTION_VIEW, 
+	    data: twitter_url 
+	});
+	var pending = Ti.Android.createPendingIntent({ 
+		'intent' : intent,
+	});
+	var notification = Ti.Android.createNotification({
+		contentIntent : pending,
+		contentTitle: 'Twitter をチェックしました',
+		contentText:   modified ? '更新されていました' : '更新されていませんでした',
+	});
+	Ti.Android.NotificationManager.notify(1, notification);
+}
+
 xhr.onload = function () {
-    var image = this.responseData;
-    Titanium.Media.Android.setSystemWallpaper(image,false);
+    var json = this.responseText;
+    var data = JSON.parse(json);
+    var tweet_id = data[0].id_str;
+    
+    var last_id = Ti.App.Properties.getString('last_tweet_id');
+    Ti.API.info([tweet_id, last_id]);
+    if (last_id && last_id == tweet_id ) {
+    	showNotification(false);
+    } else {
+    	Ti.App.Properties.setString('last_tweet_id',tweet_id);
+    	showNotification(true);
+    }
 };
 xhr.open('GET', url);
 xhr.send();
--- Resources/intent_test.js
+++ Resources/intent_test.js
@@ -3,94 +3,20 @@ var win = Ti.UI.currentWindow;
 var button1 = Ti.UI.createButton({
 	width: '100dp',
 	heigth: '50dp',
-	top: '30dp',
-	left: '10dp',
-	title: 'ACTION_SEND'
-})
-
-button1.addEventListener(
-	'click',
-	function(){
-		var intent = Ti.Android.createIntent({
-    		action: Ti.Android.ACTION_SEND,
-    		type: 'text/plain',
-    		data:'hogehuga'
-    	});
-		Ti.Android.currentActivity.startActivity(intent);
-	}
-);
-win.add(button1);
-
-var button2 = Ti.UI.createButton({
-	width: '100dp',
-	heigth: '50dp',
-	top: '90dp',
-	left: '10dp',
-	title: 'QR Reader'
-})
-
-button2.addEventListener(
-	'click',
-	function(){
-		var intent = Ti.Android.createIntent({
-    		action: "com.google.zxing.client.android.SCAN",
-    	});
-		intent.putExtra("SCAN_MODE", "QR_CODE");
-		intent.putExtra("SCAN_MODE", "EAN_8");
-		intent.putExtra("SCAN_MODE", "EAN_13");
-		Ti.Android.currentActivity.startActivity(intent);
-	}
-);
-win.add(button2);
-
-var button3 = Ti.UI.createButton({
-	width: '100dp',
-	heigth: '50dp',
-	top: '150dp',
-	left: '10dp',
-	title: 'QR Reader with Result'
-})
-
-button3.addEventListener(
-	'click',
-	function(){
-		var intent = Ti.Android.createIntent({
-			action: "com.google.zxing.client.android.SCAN"
-    	});
-		intent.putExtra("SCAN_MODE", "QR_CODE");
-		intent.putExtra("SCAN_MODE", "EAN_8");
-		intent.putExtra("SCAN_MODE", "EAN_13");
-
-		Ti.Android.currentActivity.startActivityForResult(
-			intent,
-			function(e){
-				var intent = e.intent;
-				Ti.API.debug(intent);
-				var contents = intent.getStringExtra("SCAN_RESULT");
-				alert(contents);
-			}
-		);
-	}
-);
-win.add(button3);
-
-var button4 = Ti.UI.createButton({
-	width: '100dp',
-	heigth: '50dp',
 	top: '210dp',
 	left: '10dp',
 	title: 'Service Start'
 })
 
-button4.addEventListener(
+button1.addEventListener(
 	'click',
 	function(){
 		Ti.API.debug('create intent');
 		var intent = Titanium.Android.createServiceIntent( { url: 'testservice.js' } );
-		intent.putExtra('interval', 1000000);
-		intent.putExtra('download_url', 'http://cdn-ak.f.st-hatena.com/images/fotolife/r/r_kurain/20090818/20090818232053.jpg');
+		intent.putExtra('interval', 10000);
+		intent.putExtra('twitter_id', 'kurain');
 		var service = Titanium.Android.createService(intent);
 		service.start();
 	}
 );
-win.add(button4);
+win.add(button1);
